8b99de61d35d2d1b366cc4eb55ca1cb51a750766
Kianu <kianuyepes3000@gmail.com>
2025-06-27 23:08:14 +0800

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,106 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native';
+import { Constants } from './constants/constants.js'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.container}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 60 : 0} // adjust as needed
+    >
+
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Hi! Welcome To Trikes Fare</Text>
+        </View>
+
+        <View style={styles.form}>
+
+          <TextInput
+            style={styles.textInput}
+            placeholder='Email'>
+          </TextInput>
+
+          <TextInput
+            style={styles.textInput}
+            placeholder='Passwords'
+            secureTextEntry={true}>
+          </TextInput>
+
+          <TouchableOpacity style={styles.button} onPress={() => alert('Pressed')}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+
+        </View>
+      </View>
+
+
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    position: 'relative',
+    backgroundColor: Constants.COLORS.RED,
+    flexDirection: 'column'
   },
+
+  header: {
+    flexGrow: 0.6,
+    alignItems: 'center',
+    borderWidth: 3,
+    // borderRadius: 20,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    borderColor: '#050505',
+    backgroundColor: '#E8E8E8',
+    padding: 40
+  },
+
+  headerText: {
+    marginTop: 50,
+    color: '#000',
+    fontSize: 45,
+    fontWeight: 'bold'
+  },
+
+  // Login form
+  form: {
+    position: 'absolute',
+    width: '80%',
+    height: '40%',
+    top: '50%',
+    left: '10%',
+    backgroundColor: '#FFFFFF',
+    padding: 20,
+    borderRadius: 20,
+    display: 'flex',
+    flexDirection: 'column',
+    paddingTop: 50
+  },
+
+  textInput: {
+    padding: 20,
+    backgroundColor: '#E6E1E1',
+    borderRadius: 20,
+    marginBottom: 20
+  },
+
+  // button stuff
+  button: {
+    backgroundColor: '#D33333',
+    borderRadius: 20,
+    padding: 20
+  },
+
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold'
+  }
+
 });
---
+++ constants/constants.js
@@ -0,0 +1,24 @@
+const Constants = {
+
+    COLORS: {
+        RED: '#EF4136',
+        BLACK: '#050505',
+        GRAYISH_WHITE: '#E8E8E8',
+        RED: '#D33333',
+        WHITE: '#FFFFFF'
+    },
+
+    FONT_SIZE: {
+        REGULAR: 16,
+        HEADINGS: 24,
+        LABELS: 12
+    },
+
+    BORDERS: {
+        RADIUS_NORMAL: 20,
+        RADIUS_LARGE: 40
+    }
+
+}
+
+export { Constants };
